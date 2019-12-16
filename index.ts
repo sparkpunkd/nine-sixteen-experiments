5b1b188af07bb9ee66fe494ea83c46000d696803
Richard Cartwright <spark@streampunk.media>
2019-12-16 17:58:36 +0000

    Added MJPEG output to browser image tag.

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import * as Koa from 'koa'
 export const app = new Koa()
+import { ServerResponse } from 'http'
 
 let latest: Buffer = Buffer.alloc(0)
 let building: Buffer = Buffer.alloc(0)
 const startExp = /(Content-type: image\/jpeg\r\nContent-length: (\d+)\r\n\r\n).*/
+let streams: Array<ServerResponse> = []
 
 app.use(async function(ctx) {
 	console.log(ctx)
@@ -18,7 +20,13 @@ app.use(async function(ctx) {
 			}
 			if (match) {
 				latest = building.length > 12 && building[0] === 0xff && building[1] === 0xd8 ? Buffer.from(building.slice(0, -12)) : latest
-				console.log(match[2], latest, latest.slice(-30))
+				streams.forEach(s => {
+					s.write('--jpgboundary\r\n')
+					s.write('Content-type: image/jpeg\r\n')
+					s.write(`Conent-length: ${latest.length}\r\n\r\n`)
+					s.write(latest)
+				})
+				// console.log(match[2], latest, latest.slice(-30))
 				if (+match[2] !== 11532) {
 					building = d.slice(Buffer.byteLength(match[1], 'utf8'))
 				} else {
@@ -32,10 +40,24 @@ app.use(async function(ctx) {
 		ctx.body = "Hello World"
 	}
 
-	if (ctx.method === 'GET' && ctx.path.startsWith('/latest.jpg')) {
-		ctx.type = 'image/jpeg'
-		ctx.body = latest
+	if (ctx.method === 'GET') {
+		if (ctx.path.startsWith('/latest.jpg')) {
+			ctx.type = 'image/jpeg'
+			ctx.body = latest
+		} else if (ctx.path.startsWith('/index.html')) {
+			ctx.type = 'text/html'
+			ctx.body = `<html><head></head><body>
+                    <img src="http://127.0.0.1:3000/stream.mjpg" width="100%"/>
+                  </body></html>`
+		} else if (ctx.path.startsWith('/stream.mjpg')) {
+			streams.push(ctx.res)
+			ctx.res.on('error', console.error)
+			ctx.type = 'multipart/x-mixed-replace; boundary=--jpgboundary'
+			ctx.status = 200
+			return new Promise( _resolve => { })
+		}
 	}
-});
+
+})
 
 if (!module.parent) app.listen(3000);
