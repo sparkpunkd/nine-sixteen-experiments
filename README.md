88154654a9c5f024a29a29378a3f9e8c715c1ca8
Richard Cartwright <spark@streampunk.media>
2020-01-06 18:15:39 +0000

    fix: back-pressure for response streams, better docs and better error handling

--- README.md
+++ README.md
@@ -1,25 +1,61 @@
 # Nine Sixteen Experiments
 
+This is an experiment in how to extract a stream of images from a CasparCG server for display in a web browser. This is being used a part of the development of a 9:16 cropping tool.
+
+### Install
+
 Install dependencies:
 
     yarn install
 
-Run server with:
+### Run server
+
+Run server on test port `3000` with:
 
     ts-node index.ts
 
-Run CasparCG, set up output and then ...
+### Create stream
 
-    ADD 1-99 STREAM http://127.0.0.1:3000/ -f mpjpeg -vf "scale=1024:576" -multiple_requests 1
+Run CasparCG, set up an output channel (`1` in the example) and then ...
 
-Then in a web browser, to see a single image go to ...
+    ADD 1-99 STREAM http://127.0.0.1:3000/ -f mpjpeg -multiple_requests 1
+
+Use flags `-qmin` and `-qmax` to set quality, in the range `0` to `63`, with lower numbers used to increase both quality and bitrate.
+
+To set a different framerate from that in use for the CasparCG channel, use the fps filter e.g. `-vf "fps=fps=5"`.
+
+To scale the video, use a scale filter, e.g. `-vf "scale=1024:576"`.
+
+### View the streams
+
+#### Single image
+
+In a web browser, to see a single image, go to ...
 
     http://127.0.0.1:3000/latest.jpg
 
-To watch a stream in an img tag, go to:
+This could be used in a canvas to always pull down the latest version. However, there is no handshake to determine when the image has changed.
 
-    http://127.0.0.1:3000/index.html
+#### Direct stream link
 
 Some browsers (e.g. iOS or Android) support a direct link to the stream:
 
     http://127.0.0.1:3000/stream.mjpg
+
+As the server must push every image in every stream, a maximum number of 4 concurrent streams is supported.
+
+#### Streaming in image tab
+
+To watch a stream in an `img` tag - required for some versions of Chrome - go to:
+
+    http://127.0.0.1:3000/index.html
+
+#### Examples of cropping
+
+To crop out part of an image, try this demo. The demo assumes you have a source stream that is a 2x2 multiviewer and you want to view each quadrant separately.
+
+    http://127.0.0.1:3000/cropped.html
+
+## License
+
+[MIT license](./LICENSE).
--- index.ts
+++ index.ts
@@ -5,7 +5,9 @@ import { ServerResponse } from 'http'
 let latest: Buffer = Buffer.alloc(0)
 let building: Buffer = Buffer.alloc(0)
 const startExp = /(Content-type: image\/jpeg\r\nContent-length: (\d+)\r\n\r\n).*/
-let streams: Array<ServerResponse> = []
+let streams: Array<{ r: ServerResponse, drained: boolean }> = []
+const clientLimit = 4
+let frameCounter = 0
 
 app.use(async function(ctx) {
 	console.log(ctx)
@@ -20,13 +22,21 @@ app.use(async function(ctx) {
 			}
 			if (match) {
 				latest = building.length > 12 && building[0] === 0xff && building[1] === 0xd8 ? Buffer.from(building.slice(0, -12)) : latest
-				streams.forEach(s => {
-					s.write('--jpgboundary\r\n')
-					s.write('Content-type: image/jpeg\r\n')
-					s.write(`Content-length: ${latest.length}\r\n\r\n`)
-					s.write(latest)
-				})
-				// console.log(match[2], latest, latest.slice(-30))
+				frameCounter++
+				Promise.all(streams.map((s, index) => new Promise((resolve: (b: boolean) => void, _reject) => {
+					// console.log('<<<', index, s.drained)
+					if (!s.drained) {
+						console.log(`Dropping stream ${index} frame ${frameCounter}`)
+						return resolve(s.drained)
+					}
+					s.r.write('--jpgboundary\r\n')
+					s.r.write('Content-type: image/jpeg\r\n')
+					s.r.write(`Content-length: ${latest.length}\r\n\r\n`)
+					s.drained = s.r.write(latest)
+					// console.log('>>>', index, s.drained)
+					resolve(s.drained)
+				}).catch(err => { console.error(`Failed to send JPEG to stream ${index}: ${err.message}`) })))
+
 				if (+match[2] !== 11532) {
 					building = d.slice(Buffer.byteLength(match[1], 'utf8'))
 				} else {
@@ -37,7 +47,7 @@ app.use(async function(ctx) {
 				building = Buffer.concat([building, d])
 			}
 		})
-		ctx.body = "Hello World"
+		ctx.body = `Received frame part ${frameCounter}`
 	}
 
 	if (ctx.method === 'GET') {
@@ -63,8 +73,20 @@ app.use(async function(ctx) {
 									</body>
 									</html>`
 		} else if (ctx.path.startsWith('/stream.mjpg')) {
-			streams.push(ctx.res)
-			ctx.res.on('error', console.error)
+			if (streams.length >= clientLimit) {
+				ctx.status = 429
+				ctx.body = 'Maximum number of streams exceeded.'
+				return
+			}
+			let stream = { r: ctx.res, drained: true }
+			streams.push(stream)
+			ctx.res.on('drain', () => { stream.drained = true })
+			console.log(`Streaming client connected. ${streams.length} streams now active.`)
+			ctx.res.on('error', err => { console.error(`Error for stream at index ${streams.indexOf(stream)}: ${err.message}`) })
+			ctx.res.on('close', () => {
+				streams = streams.filter(s => s !== stream)
+				console.log(`Client closed. ${streams.length} streams active.`)
+			})
 			ctx.type = 'multipart/x-mixed-replace; boundary=--jpgboundary'
 			ctx.status = 200
 			return new Promise( _resolve => { } )
@@ -73,4 +95,6 @@ app.use(async function(ctx) {
 
 })
 
-if (!module.parent) app.listen(3000, '0.0.0.0');
+if (!module.parent) app.listen(3000, '0.0.0.0', () => {
+	console.log('MJPEG forwarding server listening on port 3000.')
+});
