fadde881225d91df47db265a0440e676f827ef64
Richard Cartwright <spark@streampunk.media>
2020-01-14 11:39:53 +0000

    Testing CasparCG output on 10Gbe.

--- simples.ts
+++ simples.ts
@@ -6,7 +6,6 @@ export const app = new Koa()
 // let building = Buffer.alloc(8294400)
 // let latest = Buffer.alloc(8294400)
 // let pos = 0
-// let start = process.hrtime()
 Buffer.poolSize = 82944000
 
 // class MyIncoming extends IncomingMessage {
@@ -18,39 +17,41 @@ Buffer.poolSize = 82944000
 // }
 
 app.use(async function(ctx) {
-	console.log(ctx)
+	// console.log(ctx)
   ctx.body = 'Hello World!'
 	let start = process.hrtime()
+	let callMeDone: (x: any) => void = () => {}
 	// ctx.req.on('data', (d: Buffer) => {
-	// 	if (d.length === 11520 || pos >= building.length) {
-	// 		building.copy(latest, 0)
-	// 		console.log(pos, building.length, process.hrtime(start), ctx.req.socket.bufferSize)
-	// 		start = process.hrtime()
-	// 		// building = Buffer.alloc(8294400)
-	// 		if (d.length !== 11520) {
-	// 			pos = d.copy(building, 0)
-	// 		} else {
-	// 			pos = 0
-	// 		}
-	// 	} else {
-	// 		pos += d.copy(building, pos)
-	// 	}
+		// console.log('pos', pos, d.length)
+		// if (pos >= building.length) {
+		//
+		// 	building.copy(latest, 0)
+		// 	console.log(pos, building.length, process.hrtime(start), ctx.req.socket.bufferSize)
+		// 	start = process.hrtime()
+		// 	// building = Buffer.alloc(8294400)
+		// 	pos = d.copy(building, 0)
+		// 	callMeDone('fred')
+		// } else {
+		// 	pos += d.copy(building, pos)
+		// }
 	// setInterval(() => {
 	// 	let bufit = ctx.req.read(8294400)
 	// 	console.log(ctx.req.readable, bufit ? bufit.length : null)
 	// }, 35)
+
 	ctx.req.on('readable', () => {
-		if (ctx.req.readableLength === 11520) {
-			ctx.req.read()
-			console.log('Discarding audio')
-			return
-		}
+		// console.log('Readable length: ', ctx.req.readableLength)
 		let bufit: Buffer | null = ctx.req.read(8294400)
-		if (bufit) {
-			console.log(bufit.length, process.hrtime(start))
+		// console.log('Rabbits')
+		if (bufit !== null) {
+			console.log((bufit as Buffer).length, process.hrtime(start))
 			start = process.hrtime()
+			callMeDone('fred')
 		}
 	})
+	ctx.req.on('close', () => { console.log('Stream has ended.') })
+	ctx.req.on('error', console.error)
+	return new Promise((resolve) => { callMeDone = resolve; })
 })
 
 if (!module.parent) app.listen(3000, '0.0.0.0');
